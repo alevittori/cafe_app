ecfd662aa447be95243950834dedbc810e10fd67
alevitt <alejandrovitt@hotmail.com>
2026-09-17 12:59:37 -0300

    Comenzamos a modelar la clase cafe para usar sqlmodel, y estamos siguiendo un tutorial de la pagina oficial

--- models/cafe.py
+++ models/cafe.py
@@ -1,14 +1,44 @@
 from decimal import Decimal
-from faulthandler import is_enabled
+from sqlmodel import Field, SQLModel
+# from pydantic import BaseModel, Field
 
-from pydantic import BaseModel, Field
 
-
-class Cafe(BaseModel):
+""" class Cafe(BaseModel):
     id:int 
     name:str
     description:str
     price:Decimal = Field(max_digits=10, decimal_places=2) 
     is_enabled: bool = True
+     """
     
+class Cafe(SQLModel, table= True):
+    id: int | None = Field(default=None, primary_key = True)
+    name:  str 
+    description:str 
+    price: Decimal = Field(..., max_digits=10, decimal_places=2, ge=0)
+    available: bool= True
     
+""" Tutorial
+REcurso
+https://sqlmodel.tiangolo.com/tutorial/create-db-and-table/#primary-key-id
+
+
+
+class Hero(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str
+    secret_name: str
+    age: int | None = None
+
+Veamos ahora con más detalle 
+estas declaraciones de campos/columnas.
+
+NoneCampos, columnas que admiten valores nulos¶
+Comencemos con age, observe que tiene un tipo de int | None.
+
+Esa es la forma estándar de declarar que algo "podría ser un into un None" en Python.
+
+Y también establecimos el valor predeterminado agede None 
+
+
+"""
